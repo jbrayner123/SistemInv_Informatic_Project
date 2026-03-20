98dc755eaa936954dc2aa623f68baa6cd4753d3e
jbrayner123 <jbrayner123@gmail.com>
2026-03-20 16:37:48 -0500

    Fix

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,5 @@
 import os
 import secrets
-import certifi
 from typing import List, Optional
 from datetime import datetime
 from fastapi import FastAPI, HTTPException, Depends, Header
@@ -30,7 +29,7 @@ MONGODB_URI = os.environ.get(
     "mongodb+srv://jbrayner123:lucas300@cluster0.bczisjc.mongodb.net/?appName=Cluster0"
 )
 
-client = MongoClient(MONGODB_URI, tlsCAFile=certifi.where())
+client = MongoClient(MONGODB_URI)
 db = client["sisteminv"]
 
 col_products = db["products"]
